4ceb8f3897d69ba6063b926d5a9ac2c5b7f1b41f
Jonas Wielicki <j.wielicki@sotecware.net>
2013-03-03 16:17:44 +0100

    Render some basic PyEngine UI

--- ManiacLab/Application.py
+++ ManiacLab/Application.py
@@ -25,6 +25,10 @@ from __future__ import print_function, unicode_literals, division
 from our_future import *
 
 import argparse
+import os
+import cairo
+import sys
+import copy
 
 import _cmaniaclab as CManiacLab
 
@@ -33,23 +37,97 @@ from OpenGL.GL import *
 import Engine.Application
 import Engine.CEngine.Window as CWindow
 import Engine.CEngine.Window.key as key
+import Engine.CEngine.Pango as Pango
+import Engine.CEngine.GL as CGL
 
 from Engine.GL.Texture import Texture2D
+from Engine.VFS.FileSystem import XDGFileSystem, MountPriority
+from Engine.VFS.Mounts import MountDirectory
+from Engine.Resources.Manager import ResourceManager
+import Engine.Resources.PNGTextureLoader
+import Engine.Resources.ModelLoader
+import Engine.Resources.CSSLoader
+import Engine.Resources.MaterialLoader
+import Engine.Resources.ShaderLoader
+
+from Engine.GL import makePOT
+
+from Engine.UI import SceneWidget, VBox, HBox, LabelWidget, WindowWidget
+from Engine.UI.Theme import Theme
+import Engine.GL.Base as GL
+from Engine.UI.CSS.Rect import Rect
 
 class ManiacLab(Engine.Application.Application):
     def __init__(self):
         args = self.parse_args()
 
-        super(ManiacLab, self).__init__(CWindow.display, geometry=(500, 500))
-        self.level = CManiacLab.Level(50, 50, args.threaded_simulation)
+        display = CWindow.display
+        modes = display.DisplayModes
+        modes.sort(reverse=True)
+        displayMode = modes[0]
+        candidate = CWindow.DisplayMode(displayMode)
+        candidate.samples = 0
 
-        self.visualization = Texture2D(512, 512, GL_RGBA)
+        if candidate in modes:
+            displayMode = candidate
 
-        glClearColor(0., 0., 0., 1.);
+        candidate = CWindow.DisplayMode(displayMode)
+        candidate.stencilBits = 0
+
+        if candidate in modes:
+            displayMode = candidate
+
+        super(ManiacLab, self).__init__(CWindow.display,
+                                        geometry=(1024,768),
+                                        displayMode=displayMode)
+        del displayMode
+        del candidate
+        vfs = XDGFileSystem('maniaclab')
+        vfs.mount('/data', MountDirectory(os.path.join(os.getcwd(), "data")), MountPriority.FileSystem)
+        ResourceManager(vfs)
+
+        glClearColor(0., 0., 0.1, 1.);
         self._window.setTitle("ManiacLab")
 
-        self.running = False
+        self.theme = Theme()
+        self.theme.addRules(ResourceManager().require("ui.css"))
+
+        main_screen = self._primaryWidget
+        window = WindowWidget(self._windowLayer)
+        window.Title.Text = "Test"
+        window.AbsoluteRect.XYWH = (32, 32, 128, 128)
+
+        self.theme.applyStyles(self)
+
         self.show_thread_regions = False
+
+    def clearCairoSurface(self):
+        ctx = self._cairoContext
+        ctx.set_source_rgba(0., 0., 0., 0.)
+        ctx.set_operator(cairo.OPERATOR_SOURCE)
+        ctx.paint()
+        ctx.set_operator(cairo.OPERATOR_OVER)
+        ctx.set_line_cap(cairo.LINE_CAP_SQUARE)
+
+    def doAlign(self):
+        super(ManiacLab, self).doAlign()
+
+        mainScreen = self._primaryWidget
+
+        w, h = mainScreen.AbsoluteRect.Width, mainScreen.AbsoluteRect.Height
+        if hasattr(self, "_cairoSurface") and w == self._cairoSurface.get_width() and h == self._cairoSurface.get_height():
+            return
+        potW, potH = makePOT(w), makePOT(h)
+
+        self.cairoTexCoords = (w / potW, h / potH)
+
+        self.cairoTex = Texture2D(
+            potW, potH, format=GL_RGBA,
+            data=(GL_RGBA, GL_UNSIGNED_BYTE, None))
+        self._cairoSurface = cairo.ImageSurface(cairo.FORMAT_ARGB32, w, h)
+        self._cairoContext = cairo.Context(self._cairoSurface)
+        self._pangoContext = Pango.PangoCairoContext(self._cairoContext)
+        self.updateRenderingContext()
 
     def parse_args(self):
         parser = argparse.ArgumentParser()
@@ -64,51 +142,41 @@ class ManiacLab(Engine.Application.Application):
         return parser.parse_args()
 
     def frameSynced(self):
-        if self.running:
-            self.level.update()
-            self.level.update()
+        pass
 
     def frameUnsynced(self, timeDelta):
-        self.visualization.bind()
-        self.level.physicsToGLTexture(self.show_thread_regions)
         window = self._screens[0][0]
         window.switchTo()
-
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         glLoadIdentity()
+
         wx, wy, ww, wh = self._primaryWidget.AbsoluteRect.XYWH
+
         glViewport(0, 0, ww, wh)
         glMatrixMode(GL_PROJECTION)
         glLoadIdentity()
         glOrtho(wx, ww, wh, wy, -1., 1.)
         glMatrixMode(GL_MODELVIEW)
 
+        self.clearCairoSurface()
+        self.render()
+        self.cairoTex.bind()
+
+        s, t = self.cairoTexCoords
+        CGL.glTexCairoSurfaceSubImage2D(GL_TEXTURE_2D, 0, 0, 0, self._cairoSurface)
         glEnable(GL_TEXTURE_2D)
+        glEnable(GL_BLEND)
+        glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA)
         glBegin(GL_QUADS)
-        glTexCoord2f(0., 0.)
+        glTexCoord2f(0, 0)
         glVertex2f(0, 0)
-        glTexCoord2f(0., 250/512)
-        glVertex2f(0, 500)
-        glTexCoord2f(250/512, 250/512)
-        glVertex2f(500, 500)
-        glTexCoord2f(250/512, 0.)
-        glVertex2f(500, 0)
+        glTexCoord2f(0, t)
+        glVertex2f(0, wh)
+        glTexCoord2f(s, t)
+        glVertex2f(ww, wh)
+        glTexCoord2f(s, 0)
+        glVertex2f(ww, 0)
         glEnd()
+        Texture2D.unbind()
 
         window.flip()
-
-    def handleKeyDown(self, symbol, modifiers):
-        if symbol == key.Return:
-            self.running = not self.running
-        elif symbol == key.F1:
-            self.show_thread_regions = not self.show_thread_regions
-        elif symbol == 111:     # o
-            self.level.debug_testObject()
-        elif symbol == 115:     # s
-            self.level.debug_testStamp(44, 24)
-
-    def handleMouseDown(self, x, y, button, modifiers):
-        x = x / 800 * 50
-        y = y / 800 * 50
-        print("{0} {1}".format(x, y))
-        self.level.debug_output(x, y)
